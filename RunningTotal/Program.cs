a1a04ccef0d34513d43a512ef7c634a8d1d72371
TheFelHuntsman <129007462+TheFelHuntsman@users.noreply.github.com>
2023-06-23 16:12:50 +0100

    Added next Udemy exorcize

---
+++ RunningTotal/Program.cs
@@ -0,0 +1,19 @@
+﻿int total = new int();
+while (true)
+{
+    Console.WriteLine("Enter A number, Press Q to Quit: ");
+    string value = Console.ReadLine().ToUpper();
+    if (value == "Q")
+        break;
+    try
+    {
+       total += int.Parse(value);
+    }
+    catch
+    {
+        Console.WriteLine("please entere a valid number or Q to exit: ");
+    }  
+} Console.WriteLine($"Your total is: {total}");
+
+
+
