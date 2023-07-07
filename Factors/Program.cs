4051df5132a8a17926f849217b9fabafe1a70415
TheFelHuntsman <129007462+TheFelHuntsman@users.noreply.github.com>
2023-07-07 16:46:15 +0100

    Factors try catch UDMY

---
+++ Factors/Program.cs
@@ -0,0 +1,31 @@
+﻿int num;
+
+while (true)
+{
+
+    try
+    {
+        Console.Write("Enter a number greater than 1: ");
+        num = int.Parse(Console.ReadLine());
+        if (num > 1)
+        {
+            break;
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Invalid input, please enter number greater than 1: ");
+    }
+}
+    Console.Write($"The Factors of {num} are: ");
+
+    for (int i = 1; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            Console.WriteLine($"{i}, ");
+        }
+    }
+    Console.WriteLine($"{num}");
+
+
