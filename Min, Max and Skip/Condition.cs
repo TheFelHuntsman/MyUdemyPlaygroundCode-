959113c998f8bf4c04fb568be420f07f270cd883
TheFelHuntsman <129007462+TheFelHuntsman@users.noreply.github.com>
2023-07-07 16:10:05 +0100

    Adding in Min, Max and Skip code UDMY

---
+++ Min, Max and Skip/Condition.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min__Max_and_Skip
+{
+    internal class Condition
+    {
+        private int value1, value2, value3;
+
+        //Sets the Condition value ( += / -= )
+        public void setValue1(int value)
+        {
+            this.value1 = value;
+        }
+        public void setValue2(int value)
+        {
+            this.value2 = value;
+        } 
+        public void setValue3(int value)
+        {
+            this.value3 = value;
+        }
+
+        public bool validateValues()
+        {
+            bool retresponse = false;
+            switch (this.value2)
+            {
+                case > 0:
+                    if (value1 < value3)
+                        retresponse = true;
+                    break;
+                case < 0:
+                    if (value1 > value3)
+                        retresponse =  false;
+                    break;
+            }
+            return retresponse;
+        }
+                    
+    }
+}
+
+
+
+            
---
+++ Min, Max and Skip/Program.cs
@@ -0,0 +1,76 @@
+﻿using Min__Max_and_Skip;
+
+int init, condition, increment;
+Condition condition1 = new Condition();
+while (true)
+{
+    Console.WriteLine("Enter loop start: ");
+    try
+    {
+        init = int.Parse(Console.ReadLine());
+        condition1.setValue1(init);
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("That was not a valid int: ");
+    }
+}
+
+while (true)
+{
+    Console.WriteLine("Enter loop increment: ");
+    try
+    {
+        increment = int.Parse(Console.ReadLine());
+        if (increment == 0)
+        {
+          Console.WriteLine("Please enter a valid number: ");
+        }
+        else
+        {
+            break;
+        } 
+    }
+    catch
+    {
+        Console.WriteLine("That was not a valid int: ");
+    }
+}
+
+while (true)
+{
+    Console.WriteLine("Enter loop end: ");
+    try
+    {
+        condition = int.Parse(Console.ReadLine());
+        if (increment > 0)
+        {
+            if (condition > init)
+            {
+                for (int i = init; i <= condition; i += increment)
+                {
+                    Console.WriteLine(i);
+                } break;
+            }
+        }
+        else
+        {
+            if (condition < init)
+            {
+                {
+                    for (int i = init; i >= condition; i += increment)
+                    {
+                        Console.WriteLine(i);
+                    }break;
+                }
+            }
+        }
+        
+        
+    }
+    catch
+    {
+        Console.WriteLine("That was not a valid int: ");
+    }
+}
