9285f1103a1c0108c90058957ee12a2df146d3c5
TheFelHuntsman <129007462+TheFelHuntsman@users.noreply.github.com>
2023-09-01 19:09:44 +0100

    adding what I have for datetime / reocurring meetings for view and review

---
+++ Recurring Meeting/Program.cs
@@ -0,0 +1,82 @@
+﻿using Recurring_Meeting;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+string StartDate, EndDate;
+bool ValidValue;
+
+// method
+Meeting MeetingDetails = new Meeting();
+// params
+
+// call previous saves
+
+// start date 
+
+Console.WriteLine("Meeting Start Date & Time: ");
+StartDate = Console.ReadLine();
+MeetingDetails.setStartDate(StartDate);
+
+// end date 
+ValidValue = false;
+while (ValidValue == false)
+{
+    Console.WriteLine("Meeting End Date & time: ");
+    EndDate = Console.ReadLine();
+    Console.WriteLine("\n");
+    ValidValue = MeetingDetails.setEndDate(EndDate);
+    if (ValidValue == false)
+    {
+        Console.WriteLine("Please enter a meeting end date/time greater than the start:  ");
+    }
+}
+
+// Frequency 
+
+ValidValue = false;
+while (ValidValue == false)
+{
+    char Freq;
+    Console.WriteLine("Frequency [(D)aily,(W)eekly, (M)onthly, (Y)early]: ");
+    Freq = Console.ReadKey().KeyChar;
+    Console.WriteLine("\n");
+    ValidValue = MeetingDetails.setFrequency(Freq);
+    if (!ValidValue)
+    {
+        Console.WriteLine("Please Press a valid key: [(D)aily,(W)eekly, (M)onthly, (Y)early]: ");
+    }
+}
+// Skip Weekends
+
+ValidValue = false;
+while (ValidValue == false)
+{
+    char Skip;
+    Console.WriteLine("Do the meeting occur over the weekends [Y)es, (N)o]?: ");
+    Skip = char.Parse(Console.ReadLine());
+    Console.WriteLine("\n");
+    ValidValue = MeetingDetails.setSkip(Skip);
+    if (!ValidValue)
+    {
+        Console.WriteLine("Please eneter a valid letter [Y)es, (N)o]:");
+    }
+}
+
+// save meeting dates 
+var save = JsonSerializer.Serialize(MeetingDetails);
+string filepath = Environment.ExpandEnvironmentVariables("%Userprofile%\\Documents\\Meetings.Json");
+File.WriteAllText(filepath,save);
+// reviews what is being saved into the json
+Console.WriteLine(save);
+//Meeting MeetingDetails2 = new Meeting();
+//Writes saved json file to class (prints out to console as per saved config details) 
+/*MeetingDetails2 = JsonSerializer.Deserialize<Meeting>(save);
+Console.WriteLine(MeetingDetails2.StartDate1.ToString());*/
+
+// Print meeting dates 
+/* create class "list" 
+ * call recurrences method 
+ * add date to "list" class 
+ * break out of the list when end date it hit
+ * save class list to json from "list class"
+ * write line "list"
+*/
---
+++ Recurring Meeting/Recurrences.cs
@@ -0,0 +1,82 @@
+﻿using Recurring_Meeting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurring_Meeting
+{
+    internal class Recurrences
+    {
+        Lists Meetinglists = new Lists();
+        private DateTime meetingstarttime = new DateTime();
+        private DateTime meetingendtime = new DateTime();
+        public void Daylist(Meeting value)
+        {
+            meetingstarttime = value.StartDate1;
+            meetingendtime = value.EndDate1;
+
+            // sets the number of loops.
+            for (int i = 1; i <= 11; i++)
+            {
+                Start_EndTimes start_EndTimes = new Start_EndTimes();
+                meetingstarttime = NextMeeting(meetingstarttime, value.Freq1);
+                meetingendtime = NextMeeting(meetingendtime, value.Freq1);
+                if (!IsWeekday(meetingstarttime))
+                {
+                    while (!IsWeekday(meetingstarttime))
+                    {
+                        start_EndTimes.Start = meetingstarttime.AddDays(1);
+                        start_EndTimes.End = meetingendtime.AddDays(1);
+                    }
+                }
+                else
+                {
+                    start_EndTimes.Start = meetingstarttime;
+                    start_EndTimes.End = meetingendtime;
+                }
+                
+                Meetinglists.Times.Add(start_EndTimes);
+            }
+        }
+        // is the entered day a weekday
+        private bool IsWeekday(DateTime value)
+        {
+            if (value.DayOfWeek == DayOfWeek.Saturday || value.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private DateTime NextMeeting(DateTime time, char Freq)
+        {
+            if (Meetinglists.Times.Count == 0)
+            {
+                return time;
+            }
+            else
+            {
+                switch (Freq)
+                {
+                    case 'd':
+                        time = time.AddDays(1);
+                            return time;
+                    case 'w':
+                        time = time.AddDays(7);
+                            return time;
+                    case 'm':
+                        time = time.AddMonths(1);
+                            return time;
+                    case 'y':
+                        time = time.AddYears(1);
+                            return time;
+                }
+            }
+        }
+    }
+}
